d8045b4192175ce4a9be75ffd834ba148cae1474
Anmol-dot283 <ypoxsisofficial@gmail.com>
2020-12-07 12:06:07 +0000

    pylint: auto fixes

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -8,7 +8,6 @@ from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
 
 from userbot import ALIVE_NAME, bot
-
 from userbot.__init__ import StartTime
 from userbot.Config import Config, Var
 from userbot.utils import admin_cmd, sudo_cmd
@@ -53,4 +52,3 @@ telestats = f"{tele}"
 
 TELE_NAME = bot.me.first_name
 OWNER_ID = bot.me.id
-
--- userbot/plugins/log_pms.py
+++ userbot/plugins/log_pms.py
@@ -4,8 +4,8 @@ Check https://t.me/tgbeta/3505"""
 
 import asyncio
 
-from userbot.utils import admin_cmd
 from userbot.Config import Var
+from userbot.utils import admin_cmd
 
 NO_PM_LOG_USERS = []
 
